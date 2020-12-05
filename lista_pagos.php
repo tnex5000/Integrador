5e2be19a66ccb2c21f083f25b67c322a650ba1a3
tnex5000 <75350609+tnex5000@users.noreply.github.com>
2020-12-05 14:12:04 -0600

    Add files via upload

---
+++ lista_pagos.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'conexion.php';
+
+$datos=array();
+
+
+$query_consulta_lista_pagos="SELECT * FROM pagos WHERE ID_folio>0";
+$ejecutar= mysqli_query($connect, $query_consulta_lista_alumnos);
+
+foreach($ejecutar as $cadena){
+$datos[]=$cadena;
+}
+
+echo json_encode($datos);
